d79cc426f8230bd800718c2b8252696bb125f884
Randel M <Randel.C.Moore@gmail.com>
2020-10-23 09:45:28 -0700

    add the beginning if else logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
   $("form#inputQs").submit(function(event) {
     const coat = $("input:radio[name=coat]:checked").val();
     
-    
+    if (coat ===- coatVal1) {
+      result = "Python"
+    }
+    else {
+      result = "Java Script"
+    }
+    //const result = ...
+    //$("#outputResult").text(result);
     $(".outputText").show();
     event.preventDefault();
   });
