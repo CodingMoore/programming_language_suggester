b8550dae4f3a18ca46ddc25e54f095aeed019627
Randel M <Randel.C.Moore@gmail.com>
2020-10-23 09:32:27 -0700

    link the html form to the js function for submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
-
-
-
-
-  
+  $("form#inputQs").submit(function(event) {
+    const coat = $("input:radio[name=coat]:checked").val();
+    
+    
+    $(".outputText").show();
+    event.preventDefault();
+  });
 });
