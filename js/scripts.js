ac3725d2f401e0231a67e5d5988758043f2592dd
Randel M <Randel.C.Moore@gmail.com>
2020-10-23 14:35:43 -0700

    remove previous quiz results when the quiz is resubmited

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,12 @@ $(document).ready(function() {
     const petInt = parseInt(pet);
     const loveInt = parseInt(love);
     const answerSum = (coatInt + tvShowInt + prisonInt + petInt + loveInt);
-    alert(answerSum);
-    
+
+
+    $(".outputPython").hide();
+    $(".outputJavaScrit").hide();
+    $(".outputShakespeare").hide();
+    $(".outputDontBother").hide();
     if (answerSum >= 5 && answerSum <= 8) {
       $(".outputPython").show();
     } else if (answerSum >= 9 && answerSum <= 12) {
@@ -22,9 +26,8 @@ $(document).ready(function() {
     } else if (answerSum >= 17 && answerSum<= 20) {
       $(".outputDontBother").show();
     } else {
-      alert("missing a button")
+      alert("You seem to have left one or more of the questions un-answered.  Please answer all questions before clicking on the submit button")
     }
-    
     event.preventDefault();
   });
 });
