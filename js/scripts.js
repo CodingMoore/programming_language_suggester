5fdadc448ac009fb66ccc5b533ff112fd2d3b50b
Randel M <Randel.C.Moore@gmail.com>
2020-10-23 13:35:31 -0700

    add js paresInt logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
   $("form#inputQs").submit(function(event) {
     const coat = $("input:radio[name=coat]:checked").val();
-    const 
-
-
-
-
+    const tvShow = $("input:radio[name=tvShow]:checked").val();
+    const prison = $("input:radio[name=prison]:checked").val();
+    const pet = $("input:radio[name=pet]:checked").val();
+    const love = $("input:radio[name=love]:checked").val();
+    const coatInt = parseInt(coat);
+    const tvShowInt = parseInt(tvShow);
+    const prisonInt = parseInt(prison);
+    const petInt = parseInt(pet);
+    const loveInt = parseInt(love);
+    const answerSum = (coatInt + tvShowInt + prisonInt + petInt + loveInt);
+    alert(answerSum);
+    //alert(coatInt);
+    //alert(tvShowInt);
+    //alert(prisonInt);
+    //alert(petInt);
+    //alert(loveInt);
+    //alert(coatInt + tvShowInt + prisonInt + petInt + loveInt);
     
-    //if (coat === coatVal1) {
-    //  result = "Python"
-    //}
-    //else {
-    //  result = "Java Script"
-    //}
-    //const result = ...
+
     
     $("#outputResult").text(result);
     $(".outputText").show();
